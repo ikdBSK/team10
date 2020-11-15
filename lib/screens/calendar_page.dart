a027827bc174fd87378557147fc2e111de48ef12
ponmas <honda@sens.sys.es.osaka-u.ac.jp>
2020-11-15 12:32:01 +0900

    スタンプカードとスタンプの表示

--- lib/screens/calendar_page.dart
+++ lib/screens/calendar_page.dart
@@ -7,9 +7,17 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  CrossFadeState _crossFadeState = CrossFadeState.showFirst;
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+    final image0 = Image.asset(
+      "lib/images/Black Routine Transparent.png",
+      color: Colors.white.withOpacity(0.0),
+    );
+    final image1 = Image.asset(
+      "lib/images/Black Routine Transparent.png",
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text('今日の成果'),
@@ -23,38 +31,76 @@ class _CalendarPageState extends State<CalendarPage> {
                 children: <Widget>[
                   Image.asset(
                     "lib/images/card.jpg",
-                    width: double.infinity,
+                    //width: double.infinity,
+                    fit: BoxFit.cover,
                   ),
                   Positioned(
                     top: 260.0,
                     left: 0.0,
-                    width: size.width/4,
-                    height: size.width/4,
+                    width: size.width / 4,
+                    height: size.width / 4,
                     child: Image.asset(
-                      "lib/images/Black Routine.png",
+                      "lib/images/Black Routine Transparent.png",
                       height: 50,
                     ),
                   ),
                   Positioned(
                     top: 260.0,
-                    left: size.width/4,
-                    width: size.width/4,
-                    height: size.width/4,
+                    left: size.width / 4,
+                    width: size.width / 4,
+                    height: size.width / 4,
                     child: Image.asset(
-                      "lib/images/Black Routine.png",
+                      "lib/images/Black Routine Transparent.png",
                       height: 50,
                     ),
                   ),
                   Positioned(
                     top: 260.0,
-                    left: 2*size.width/4,
-                    width: size.width/4,
-                    height: size.width/4,
+                    left: 2 * size.width / 4,
+                    width: size.width / 4,
+                    height: size.width / 4,
                     child: Image.asset(
-                      "lib/images/Black Routine.png",
+                      "lib/images/Black Routine Transparent.png",
                       height: 50,
                     ),
                   ),
+                  Positioned(
+                    top: 260.0,
+                    left: 3 * size.width / 4,
+                    width: size.width / 4,
+                    height: size.width / 4,
+                    child: Image.asset(
+                      "lib/images/Black Routine Transparent.png",
+                      height: 50,
+                    ),
+                  ),
+                  Positioned(
+                    top: 260.0 + size.width / 4,
+                    left: 0.0,
+                    width: size.width / 4,
+                    height: size.width / 4,
+                    child: AnimatedCrossFade(
+                      firstChild: image0,
+                      secondChild: image1,
+                      duration: Duration(seconds: 0),
+                      crossFadeState: _crossFadeState,
+                    ),
+                  ),//スタンプを押せる
+                  Positioned(
+                    top: 260.0 + size.width / 4,
+                    left: 0.0,
+                    width: size.width/5,
+                    height: size.width/5,
+                    child: RaisedButton(
+                      //child: const Text('Btn'),
+                      color: Colors.white.withOpacity(0.0),
+                      onPressed: () {
+                        setState(() {
+                          _crossFadeState = CrossFadeState.showSecond;
+                        });
+                      },
+                    ),
+                  ),//ボタン
                 ],
               ),
             ),
