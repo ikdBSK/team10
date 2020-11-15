ea9488374d19a971de58a56d1d2b5ab4fee3671c
Tatsuki Ikeda <ocharoman924@gmail.com>
2020-11-15 13:34:27 +0900

    タイマー実装

--- lib/components/drawer_menu.dart
+++ lib/components/drawer_menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:app_team10/screens/setting_page.dart';
+import 'package:app_team10/screens/timer_page.dart';
+import 'package:app_team10/screens/calendar_page.dart';
 
 class DrawerMenu extends StatelessWidget {
   @override
@@ -25,6 +27,28 @@ class DrawerMenu extends StatelessWidget {
               context,
               MaterialPageRoute(
                 builder: (content) => SettingPage(),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          title: Text('Timer'),
+          onTap: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (content) => TimerPage(),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          title: Text('Calendar'),
+          onTap: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (content) => CalendarPage(),
               ),
             );
           },
